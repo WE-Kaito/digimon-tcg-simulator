79e594a5ad28dd4ba31f79f10dcc7a2b84a7af7d
Enrico <e.weissenrieder94@gmail.com>
2023-07-25 02:50:00 +0200

    removed unused import

--- backend/src/main/java/com/github/wekaito/backend/BackendApplication.java
+++ backend/src/main/java/com/github/wekaito/backend/BackendApplication.java
@@ -2,7 +2,6 @@ package com.github.wekaito.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class BackendApplication {
