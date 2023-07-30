58c35c3b099d52130ff9f31a5b8df63368222873
Enrico <e.weissenrieder94@gmail.com>
2023-07-30 13:42:24 +0200

    coverage

--- backend/src/test/java/com/github/wekaito/backend/DeckTest.java
+++ backend/src/test/java/com/github/wekaito/backend/DeckTest.java
@@ -1,31 +1,50 @@
 package com.github.wekaito.backend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class DeckTest {
 
-    @Test
-    void testEquals() {
-        Card card1 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
-        Card card2 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
-
-        Card card3 = new Card("Gabumon", "Digimon", "Blue", "https://images.digimoncard.io/images/cards/BT1-011.jpg", "BT1-011", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
-
-        Deck deck1 = new Deck("1", "New Deck", new Card[]{card1, card2});
-        Deck deck2 = new Deck("1", "New Deck", new Card[]{card1, card2});
-        Deck deck3 = new Deck("2", "Another Deck", new Card[]{card1, card2});
-        Deck deck4 = new Deck("1", "New Deck", new Card[]{card1, card3});
-
-        assertEquals(deck1, deck2);
-        assertNotEquals(deck1, deck3);
-        assertNotEquals(deck1, deck4);
-        assertNotEquals(null, deck1);
+    @ParameterizedTest
+    @MethodSource("provideDecksForEqualityTest")
+    void testEquals(Deck deck1, Deck deck2, boolean expectedEquals) {
+        assertEquals(expectedEquals, deck1.equals(deck2));
     }
 
-    @Test
-    void testHashCode() {
+    @ParameterizedTest
+    @MethodSource("provideDecksForHashCodeTest")
+    void testHashCode(Deck deck1, Deck deck2, boolean expectedEquals) {
+        if (expectedEquals) {
+            assertEquals(deck1.hashCode(), deck2.hashCode());
+        } else {
+            assertNotEquals(deck1.hashCode(), deck2.hashCode());
+        }
+    }
+
+    private static Stream<Arguments> provideDecksForEqualityTest() {
+        Card card1 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
+        Card card2 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
+
+        Deck deck1 = new Deck("1", "New Deck", new Card[]{card1, card2});
+        Deck deck2 = new Deck("1", "New Deck", new Card[]{card1, card2});
+        Deck deck3 = new Deck("2", "Another Deck", new Card[]{card1, card2});
+        Deck deck4 = new Deck("1", "New Deck", new Card[]{card1});
+
+        return Stream.of(
+                Arguments.of(deck1, deck2, true),
+                Arguments.of(deck1, deck3, false),
+                Arguments.of(deck1, deck4, false)
+        );
+    }
+
+    private static Stream<Arguments> provideDecksForHashCodeTest() {
         Card card1 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
         Card card2 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
 
@@ -33,7 +52,9 @@ class DeckTest {
         Deck deck2 = new Deck("1", "New Deck", new Card[]{card1, card2});
         Deck deck3 = new Deck("2", "Another Deck", new Card[]{card1, card2});
 
-        assertEquals(deck1.hashCode(), deck2.hashCode());
-        assertNotEquals(deck1.hashCode(), deck3.hashCode());
+        return Stream.of(
+                Arguments.of(deck1, deck2, true),
+                Arguments.of(deck1, deck3, false)
+        );
     }
 }
--- backend/src/test/java/com/github/wekaito/backend/DeckWithoutIdTest.java
+++ backend/src/test/java/com/github/wekaito/backend/DeckWithoutIdTest.java
@@ -1,39 +1,64 @@
 package com.github.wekaito.backend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class DeckWithoutIdTest {
 
-    @Test
-    void testEquals() {
-        Card card1 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
-        Card card2 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
-
-        Card card3 = new Card("Gabumon", "Digimon", "Blue", "https://images.digimoncard.io/images/cards/BT1-011.jpg", "BT1-011", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
-
-        DeckWithoutId deck1 = new DeckWithoutId("New Deck", new Card[]{card1, card2});
-        DeckWithoutId deck2 = new DeckWithoutId("New Deck", new Card[]{card1, card2});
-        DeckWithoutId deck3 = new DeckWithoutId("Another Deck", new Card[]{card1, card2});
-        DeckWithoutId deck4 = new DeckWithoutId("New Deck", new Card[]{card1, card3});
-
-        assertEquals(deck1, deck2);
-        assertNotEquals(deck1, deck3);
-        assertNotEquals(deck1, deck4);
-        assertNotEquals(null, deck1);
+    @ParameterizedTest
+    @MethodSource("provideDecksForEqualityTest")
+    void testEquals(DeckWithoutId deck1, DeckWithoutId deck2, boolean expectedEquals) {
+        assertEquals(expectedEquals, deck1.equals(deck2));
     }
 
-    @Test
-    void testHashCode() {
+    @ParameterizedTest
+    @MethodSource("provideDecksForHashCodeTest")
+    void testHashCode(DeckWithoutId deck1, DeckWithoutId deck2, boolean expectedEquals) {
+        if (expectedEquals) {
+            assertEquals(deck1.hashCode(), deck2.hashCode());
+        } else {
+            assertNotEquals(deck1.hashCode(), deck2.hashCode());
+        }
+    }
+
+    private static Stream<Arguments> provideDecksForEqualityTest() {
         Card card1 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
         Card card2 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
 
         DeckWithoutId deck1 = new DeckWithoutId("New Deck", new Card[]{card1, card2});
         DeckWithoutId deck2 = new DeckWithoutId("New Deck", new Card[]{card1, card2});
         DeckWithoutId deck3 = new DeckWithoutId("Another Deck", new Card[]{card1, card2});
+        DeckWithoutId deck4 = new DeckWithoutId("New Deck", new Card[]{});
+        DeckWithoutId deck5 = new DeckWithoutId("New Deck", new Card[]{card2, card1});
 
-        assertEquals(deck1.hashCode(), deck2.hashCode());
-        assertNotEquals(deck1.hashCode(), deck3.hashCode());
+        return Stream.of(
+                Arguments.of(deck1, deck2, true),
+                Arguments.of(deck1, deck3, false),
+                Arguments.of(deck1, deck4, false),
+                Arguments.of(deck1, deck5, true)
+        );
+    }
+
+    private static Stream<Arguments> provideDecksForHashCodeTest() {
+        Card card1 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
+        Card card2 = new Card("Agumon", "Digimon", "Red", "https://images.digimoncard.io/images/cards/BT1-010.jpg", "BT1-010", "Rookie", "Vaccine", "Reptile", 2000, 3, 0, 3, "[On Play] Reveal 5 cards from the top of your deck. Add 1 Tamer card among them to your hand. Place the remaining cards at the bottom of your deck in any order.", null);
+
+        DeckWithoutId deck1 = new DeckWithoutId("New Deck", new Card[]{card1, card2});
+        DeckWithoutId deck2 = new DeckWithoutId("New Deck", new Card[]{card1, card2});
+        DeckWithoutId deck3 = new DeckWithoutId("Another Deck", new Card[]{card1, card2});
+        DeckWithoutId deck4 = new DeckWithoutId("New Deck", new Card[]{});
+
+        return Stream.of(
+                Arguments.of(deck1, deck2, true),
+                Arguments.of(deck1, deck3, false),
+                Arguments.of(deck1, deck4, false)
+        );
     }
 }
