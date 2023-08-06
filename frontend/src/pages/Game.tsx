fef655e0903c7cb9c481d3a2b55a52d3a34e8cd3
Enrico <e.weissenrieder94@gmail.com>
2023-08-06 20:10:41 +0200

    refactor

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -6,24 +6,25 @@ import {useGame} from "../hooks/useGame.ts";
 import {profilePicture} from "../utils/functions.ts";
 import {useEffect, useState} from "react";
 import styled from "@emotion/styled";
+import SurrenderMoodle from "../components/game/SurrenderMoodle.tsx";
 
 export default function Game({user}: { user: string }) {
 
     const currentPort = window.location.port;
     const websocketURL = currentPort === "5173" ? "ws://localhost:8080/api/ws/game" : "wss://cgn-java-23-2-enrico.capstone-project.de/api/ws/chat";
+    const navigate = useNavigate();
 
     const gameId = useStore((state) => state.gameId);
     const setUpGame = useGame((state) => state.setUpGame);
-    const navigate = useNavigate();
 
     const myAvatar = useGame((state) => state.myAvatar);
     const opponentAvatar = useGame((state) => state.opponentAvatar);
     const opponentName = useGame((state) => state.opponentName);
 
-    const [surrenderOpen, setSurrenderOpen] = useState(false);
-    const [timerOpen, setTimerOpen] = useState(false);
-    const [timer, setTimer] = useState(5);
-    const [opponentLeft, setOpponentLeft] = useState(false);
+    const [surrenderOpen, setSurrenderOpen] = useState<boolean>(false);
+    const [timerOpen, setTimerOpen] = useState<boolean>(false);
+    const [timer, setTimer] = useState<number>(5);
+    const [opponentLeft, setOpponentLeft] = useState<boolean>(false);
 
     const websocket = useWebSocket(websocketURL, {
 
@@ -41,11 +42,11 @@ export default function Game({user}: { user: string }) {
                 setUpGame(me, opponent);
             }
 
-            if (event.data.startsWith("[SURRENDER]")) {
+            if (event.data === "[SURRENDER]") {
                 startTimer();
             }
 
-            if (event.data.startsWith("[PLAYER_LEFT]")) {
+            if (event.data === "[PLAYER_LEFT]") {
                 setOpponentLeft(true);
                 startTimer();
             }
@@ -57,7 +58,7 @@ export default function Game({user}: { user: string }) {
         setTimer(5);
         setTimerOpen(false);
         setSurrenderOpen(false);
-        }, []);
+    }, []);
 
     useEffect(() => {
         if (timer === 0) navigate("/lobby");
@@ -79,103 +80,24 @@ export default function Game({user}: { user: string }) {
 
     return (
         <>
-            {(surrenderOpen || timerOpen) &&
-                <SurrenderMoodle>
-                    {!timerOpen ?
-                        (<>
-                            <SurrenderSpan>Do you really want to surrender?</SurrenderSpan>
-                            <div style={{width: 390, display: "flex", justifyContent: "space-between"}}>
-                                <CancelSurrenderButton
-                                    onClick={() => setSurrenderOpen(!surrenderOpen)}>CANCEL</CancelSurrenderButton>
-                                <SurrenderButton onClick={handleSurrender}>SURRENDER</SurrenderButton>
-                            </div>
-                        </>)
-                        :
-                        (<>
-                            <SurrenderSpan>{surrenderOpen ? "You surrendered." : `${opponentLeft ? "Opponent left." : "You win!"}`} GAME ENDING...</SurrenderSpan>
-                            <SurrenderSpan style={{fontFamily:"Awsumsans, sans-serif"}}>{timer}</SurrenderSpan>
-                        </>)
-                    }
-                </SurrenderMoodle>}
+        {(surrenderOpen || timerOpen) &&
+            <SurrenderMoodle timer={timer} timerOpen={timerOpen} surrenderOpen={surrenderOpen}
+                             setSurrenderOpen={setSurrenderOpen} opponentLeft={opponentLeft}
+                             handleSurrender={handleSurrender}/>}
 
-            <h1>Game</h1>
+        <h1>Game</h1>
 
-            <div style={{display: "flex", flexDirection: "column"}}>
-                <span>{opponentName}</span>
-                <img alt="opponent" src={profilePicture(opponentAvatar)}/>
-            </div>
-            <h2>VS.</h2>
-            <div style={{display: "flex", flexDirection: "column"}}>
-                <span>{user}</span>
-                <img alt="me" src={profilePicture(myAvatar)} onClick={() => setSurrenderOpen(!surrenderOpen)}
-                     style={{cursor: "pointer"}}/>
-            </div>
-
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <span>{opponentName}</span>
+            <img alt="opponent" src={profilePicture(opponentAvatar)}/>
+        </div>
+        <h2>VS.</h2>
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <span>{user}</span>
+            <img alt="me" src={profilePicture(myAvatar)} onClick={() => setSurrenderOpen(!surrenderOpen)}
+                 style={{cursor: "pointer"}}/>
+        </div>
         </>
     );
 }
 
-const SurrenderMoodle = styled.div`
-  position: absolute;
-  width: 560px;
-  height: 200px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 30px;
-  background: #0c0c0c;
-  transition: all 0.2s ease;
-
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const SurrenderSpan = styled.span`
-  font-family: Pixel Digivolve, sans-serif;
-  font-size: 24px;
-  text-shadow: black 2px 4px 2px;
-`;
-
-const CancelSurrenderButton = styled.button`
-  cursor: pointer;
-  width: 160px;
-  height: 50px;
-  flex-shrink: 0;
-  border-radius: 0;
-  background: #D9D9D9;
-  font-family: Pixel Digivolve, sans-serif;
-  font-size: 20px;
-  color: #0c0c0c;
-  box-shadow: 6px 12px 1px 0 rgb(0, 0, 0);
-  transition: all 0.15s ease;
-
-  &:hover {
-    background: lightgray;
-    transform: translateY(1px);
-    box-shadow: 4px 8px 1px 0 rgba(0, 0, 0, 0.9);
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:active {
-    background: #f8f8f8;
-    transform: translateY(2px);
-    box-shadow: 2px 4px 1px 0 rgba(0, 0, 0, 0.8);
-  }
-`;
-
-const SurrenderButton = styled(CancelSurrenderButton)`
-  background-color: #c03427;
-
-  &:hover {
-    background: #da483b;
-  }
-
-  &:active {
-    background: #e72737;
-  }
-`;
