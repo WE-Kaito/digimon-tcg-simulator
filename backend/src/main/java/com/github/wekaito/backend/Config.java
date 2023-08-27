1b2204c39517515d9e063f9210f07cfa473b7158
Enrico <e.weissenrieder94@gmail.com>
2023-08-27 21:15:27 +0200

    added config

---
+++ backend/src/main/java/com/github/wekaito/backend/Config.java
@@ -0,0 +1,37 @@
+package com.github.wekaito.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(
+                            String resourcePath,
+                            Resource location
+                    ) throws IOException {
+                        Resource requestedResource = location.createRelative(
+                                resourcePath
+                        );
+
+                        return (
+                                requestedResource.exists() &&
+                                        requestedResource.isReadable()
+                        ) ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
+}
