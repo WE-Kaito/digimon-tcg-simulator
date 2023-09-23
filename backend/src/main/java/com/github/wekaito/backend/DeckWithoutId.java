eeaadd04cb91a15fbfe46f1442845dc01d94c4aa
Enrico <e.weissenrieder94@gmail.com>
2023-09-23 03:44:40 +0200

    decklist validation

--- backend/src/main/java/com/github/wekaito/backend/DeckWithoutId.java
+++ backend/src/main/java/com/github/wekaito/backend/DeckWithoutId.java
@@ -1,10 +1,14 @@
 package com.github.wekaito.backend;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
 import java.util.List;
 
 public record DeckWithoutId(
         String name,
         String color,
+        @Min(50)
+        @Max(55)
         List<String> decklist) {
-
 }
