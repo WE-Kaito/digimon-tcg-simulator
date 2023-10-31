75306438bae51719746e53da476cac9c7d9f42dc
Enrico <e.weissenrieder94@gmail.com>
2023-10-31 15:20:23 +0100

    patchnotes

--- frontend/src/pages/MainMenu.tsx
+++ frontend/src/pages/MainMenu.tsx
@@ -18,7 +18,7 @@ export default function MainMenu() {
                 <MenuButton name={"LOGOUT"} path={"/login"}/>
             </Container>
             <PatchNotes><PatchNotesText>
-                <u>Patchnotes (15/10)</u>
+                <u>Patchnotes</u> (15/10)
                 <ul>
                     <li>• <span>Drag card stacks with the new icon on hovered cards, starting from the 2. stacked card (for now, the to-be-replaced tamer are not included)</span>
                     </li>
@@ -26,7 +26,7 @@ export default function MainMenu() {
                     <li>• <span>Directly drop cards to deck top/bottom</span></li>
                     <li>• <span>Right click deck to open the option to retrieve the bottom card</span></li>
                     <li>• <span>⨯/+ - Buttons on hovered cards in Deckbuilder</span></li>
-                    <li>• <span style={{color: "hotpink"}}>Replaced avatars with 20 sprites by Tortoiseshel</span></li>
+                    <li>• <span>Replaced avatars with 20 sprites by Tortoiseshel</span></li>
                     <li>• Trash/Deck(top/bottom)/Stack-bottom now marked as droppable, when hovering with dragged item
                     </li>
                     <li>• Log: Memory now [old ➟ new]; Security shuffle</li>
@@ -34,6 +34,20 @@ export default function MainMenu() {
                     <li>• Max username length now 16 chars</li>
                     <li>• additional backend validation should prevent broken deck data</li>
                     <li>• minor fixes</li>
+                </ul>
+                (31/10)
+                <ul>
+                    <li>• <span>32 Card Sleeves by Drak</span></li>
+                    <li>• <span>10 more avatars by Tortoiseshel</span></li>
+                    <li>• <span>Password Recovery:</span>
+                    <br/> <span style={{color: "crimson", paddingLeft: 7}}>Please add a secure safety question in Profile!</span></li>
+                    <li>• <span>Right click on a hand card to reveal it</span></li>
+                    <li>• <span>Right click on details card image to open in new tab</span></li>
+                    <li>• Replaced background pictures with tsParticles</li>
+                    <li>• Timer and Auto-Redirect removed from game</li>
+                    <li>• Mulligan and other actions should not be able to alter the position of opponents cards anymore</li>
+                    <li>• minor adjustments and fixes</li>
+                    <li>• <span style={{color: "#1d7dfc"}}>dev:</span> code cleanup and "how to run locally" md file</li>
                 </ul>
             </PatchNotesText></PatchNotes>
         </Wrapper>
@@ -63,8 +77,8 @@ const PatchNotes = styled.div`
   position: absolute;
   top: 0;
   right: 20px;
-  width: 300px;
-  height: 400px;
+  width: 295px;
+  height: 650px;
   background: rgba(7, 13, 17, 0.9);
   filter: drop-shadow(0 0 3px #1d7dfc);
   border-radius: 10px;
