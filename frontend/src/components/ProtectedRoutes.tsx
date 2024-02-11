39c648bc27e0a01cebda92b080215e20eb99a3a5
Rustywolf <dawsoking2@hotmail.com>
2024-02-11 23:20:19 +1100

    Remove unintended changes; Prevent local run of frontend from failing

--- frontend/src/components/ProtectedRoutes.tsx
+++ frontend/src/components/ProtectedRoutes.tsx
@@ -2,7 +2,7 @@ import {Outlet, Navigate} from "react-router-dom";
 
 export default function ProtectedRoutes({user}: { user?: string }) {
 
-    if (user === "") return "loading3 ...";
+    if (user === "") return "loading ...";
 
     const isLoggedIn = user !== "anonymousUser";
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+let backendUrl = 'http://localhost:8080';
+if (process.env.BACKEND_URL) {
+  backendUrl = process.env.BACKEND_URL;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://backend:8080',
+      '/api': backendUrl,
     },
   },
 })
