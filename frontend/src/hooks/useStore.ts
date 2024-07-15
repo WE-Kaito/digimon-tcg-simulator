6ed6aba4e496ccfa4edc6d1c60d9c19a6813d3b6
Enrico <e.weissenrieder94@gmail.com>
2024-07-15 02:00:59 +0200

    * fix

--- frontend/src/hooks/useStore.ts
+++ frontend/src/hooks/useStore.ts
@@ -19,7 +19,6 @@ import { avatars } from "../utils/avatars.ts";
 import {DeckIdOrder} from "../pages/Profile.tsx";
 import {Dispatch, SetStateAction} from "react";
 import {generalToken} from "../utils/tokens.ts";
-import {Container} from "@tsparticles/engine";
 
 type State = {
     fetchedCards: CardTypeWithId[],
