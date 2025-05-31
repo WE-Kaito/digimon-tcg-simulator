ed5f40882f0c0dd0828a131f041c9332c56470ef
Kaito <e.weissenrieder94@gmail.com>
2025-05-31 03:40:28 +0200

    * update patchnotes

--- frontend/src/components/PatchnotesLink.tsx
+++ frontend/src/components/PatchnotesLink.tsx
@@ -2,35 +2,38 @@ import styled from "@emotion/styled";
 
 export default function PatchnotesLink() {
     return (
-        <Wrapper >
-            <Patchnotes href={"https://github.com/WE-Kaito/digimon-tcg-simulator/wiki/Patchnotes"}
-                        target="_blank" rel="noopener noreferrer">
-                last patch: 24.12.2024
+        <Wrapper>
+            <Patchnotes
+                href={"https://github.com/WE-Kaito/digimon-tcg-simulator/wiki/Patchnotes"}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                last patch: 31.05.2025
             </Patchnotes>
         </Wrapper>
     );
 }
 
 const Wrapper = styled.sub`
-  width: 100vw;
-  position: absolute;
-  bottom: 10px;
-  left: 50%;
-  transform: translateX(-50%);
-  font-family: Cousine, monospace;
+    width: 100vw;
+    position: absolute;
+    bottom: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+    font-family: Cousine, monospace;
 `;
 
 const Patchnotes = styled.a`
-  color: whitesmoke;
-  text-decoration: none;
-
-  &:hover {
-    color: #daa600 !important;
-  }
-  &:visited {
     color: whitesmoke;
-  }
-  @media (max-width: 550px) {
-    font-size: 0.8em;
-  }
+    text-decoration: none;
+
+    &:hover {
+        color: #daa600 !important;
+    }
+    &:visited {
+        color: whitesmoke;
+    }
+    @media (max-width: 550px) {
+        font-size: 0.8em;
+    }
 `;
