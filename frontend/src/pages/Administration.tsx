7aed3cead2e78847a45a7ee63b769d84e9e4d65c
Kaito <e.weissenrieder94@gmail.com>
2026-01-02 19:26:19 +0100

    add loading animation to admin page

--- frontend/src/pages/Administration.tsx
+++ frontend/src/pages/Administration.tsx
@@ -6,6 +6,8 @@ import { useGeneralStates } from "../hooks/useGeneralStates.ts";
 import useQuery from "../hooks/useQuery.ts";
 import { Navigate } from "react-router-dom";
 import ServerMessageInput from "../components/administration/ServerMessageInput.tsx";
+import Lottie from "lottie-react";
+import loadingAnimation from "../assets/lotties/loading.json";
 
 export default function Administration() {
     const user = useGeneralStates((state) => state.user);
@@ -28,7 +30,11 @@ export default function Administration() {
             >
                 <SectionHeadline headline={"Administration"} rightElement={<BackButton />} />
 
-                {!isFetching && (
+                {isFetching ? (
+                    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                        <Lottie animationData={loadingAnimation} loop={true} style={{ width: "50%" }} />
+                    </div>
+                ) : (
                     <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
                         <BannedUsers />
                         <ServerMessageInput />
