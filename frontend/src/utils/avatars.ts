ea28aee22bccb53ff8f592239e71d9fdcf1f8837
Kaito <e.weissenrieder94@gmail.com>
2026-01-17 10:33:44 +0100

    add new avatars

--- frontend/src/utils/avatars.ts
+++ frontend/src/utils/avatars.ts
@@ -69,6 +69,66 @@ import shoemonSrc from "../assets/profile_pictures/SHOEMON.png";
 import searcherSrc from "../assets/profile_pictures/searcher.png";
 import eaterSrc from "../assets/profile_pictures/eater.png";
 import motherSrc from "../assets/profile_pictures/Mother.png";
+import agumon2006 from "../assets/profile_pictures/Agumon_2006.png";
+import agumonYnK from "../assets/profile_pictures/Agumon_YnK.png";
+import bakumon from "../assets/profile_pictures/Bakumon.png";
+import beelzebumonBlast from "../assets/profile_pictures/Beelzebumon_Blast.png";
+import belphenonRage from "../assets/profile_pictures/Belphemon_Rage.png";
+import bitmon from "../assets/profile_pictures/Bitmon.png";
+import cyberdramon from "../assets/profile_pictures/Cyberdramon.png";
+import dagomon from "../assets/profile_pictures/Dagomon.png";
+import digitamamon from "../assets/profile_pictures/Digitamamon.png";
+import dracmon from "../assets/profile_pictures/Dracmon.png";
+import duftmon from "../assets/profile_pictures/Duftmon.png";
+import dukemonX from "../assets/profile_pictures/Dukemon_X.png";
+import duskmon from "../assets/profile_pictures/Duskmon.png";
+import elecmon from "../assets/profile_pictures/Elecmon.png";
+import etemon from "../assets/profile_pictures/Etemon.png";
+import galgomon from "../assets/profile_pictures/Galgomon.png";
+import gaogamon from "../assets/profile_pictures/Gaogamon.png";
+import gaomon from "../assets/profile_pictures/Gaomon.png";
+import garudamon from "../assets/profile_pictures/Garudamon.png";
+import guardromon from "../assets/profile_pictures/Guardromon.png";
+import hangyomon from "../assets/profile_pictures/Hangyomon.png";
+import herissmon from "../assets/profile_pictures/Herissmon.png";
+import holydramon from "../assets/profile_pictures/Holydramon.png";
+import iceDevimon from "../assets/profile_pictures/IceDevimon.png";
+import ikkakumon from "../assets/profile_pictures/Ikkakumon.png";
+import jyureimon from "../assets/profile_pictures/Jyureimon.png";
+import knightmon from "../assets/profile_pictures/Knightmon.png";
+import koemon from "../assets/profile_pictures/Koemon.png";
+import kougamon from "../assets/profile_pictures/Kougamon.png";
+import lalamon from "../assets/profile_pictures/Lalamon.png";
+import lucemonFalldown from "../assets/profile_pictures/Lucemon_Falldown.png";
+import mastemon from "../assets/profile_pictures/Mastemon.png";
+import megaSeadramonX from "../assets/profile_pictures/MegaSeadramon_X.png";
+import megadramon from "../assets/profile_pictures/Megadramon.png";
+import megaloGrowmon from "../assets/profile_pictures/MegaloGrowmon.png";
+import megidramon from "../assets/profile_pictures/Megidramon.png";
+import meramon from "../assets/profile_pictures/Meramon.png";
+import metalGarurumonX from "../assets/profile_pictures/MetalGarurumon_X.png";
+import metalGreymonVirus from "../assets/profile_pictures/MetalGreymon_Virus.png";
+import minervamonX from "../assets/profile_pictures/Minervamon_X.png";
+import monzaemon from "../assets/profile_pictures/Monzaemon.png";
+import noblePumpmon from "../assets/profile_pictures/NoblePumpmon.png";
+import ophanimonCore from "../assets/profile_pictures/Ophanimon_Core.png";
+import ordinemon from "../assets/profile_pictures/Ordinemon.png";
+import paledramon from "../assets/profile_pictures/Paledramon.png";
+import palmon from "../assets/profile_pictures/Palmon.png";
+import penmon from "../assets/profile_pictures/Penmon.png";
+import plotmon from "../assets/profile_pictures/Plotmon.png";
+import rapidmonArmor from "../assets/profile_pictures/Rapidmon_Armor.png";
+import renamon from "../assets/profile_pictures/Renamon.png";
+import submarimon from "../assets/profile_pictures/Submarimon.png";
+import tentomon from "../assets/profile_pictures/Tentomon.png";
+import toyAgumon from "../assets/profile_pictures/ToyAgumon.png";
+import turuiemon from "../assets/profile_pictures/Turuiemon.png";
+import ulforceVdramon from "../assets/profile_pictures/UlforceV-dramon.png";
+import valdurmon from "../assets/profile_pictures/Valdurmon.png";
+import velgrmon from "../assets/profile_pictures/Velgrmon.png";
+import warGreymonX from "../assets/profile_pictures/WarGreymon_X.png";
+import yatagaramon2006 from "../assets/profile_pictures/Yatagaramon_2006.png";
+import gekkomon from "../assets/profile_pictures/GEKKOMON.png";
 
 export const avatars: Picture[] = [
     { name: "AncientIrismon", imagePath: ancientIrismon, artist: "Tortoiseshel" },
@@ -129,6 +189,65 @@ export const avatars: Picture[] = [
     { name: "Blossomon", imagePath: blossomon, artist: "Tortoiseshel" },
     { name: "Delumon", imagePath: delumon, artist: "Tortoiseshel" },
     { name: "Andiramon Data", imagePath: andiramonData, artist: "Tortoiseshel" },
+    { name: "Agumon 2006", imagePath: agumon2006, artist: "Tortoiseshel" },
+    { name: "Agumon YnK", imagePath: agumonYnK, artist: "Tortoiseshel" },
+    { name: "Bakumon", imagePath: bakumon, artist: "Tortoiseshel" },
+    { name: "Beelzebumon Blast", imagePath: beelzebumonBlast, artist: "Tortoiseshel" },
+    { name: "Belphemon Rage", imagePath: belphenonRage, artist: "Tortoiseshel" },
+    { name: "Bitmon", imagePath: bitmon, artist: "Tortoiseshel" },
+    { name: "Cyberdramon", imagePath: cyberdramon, artist: "Tortoiseshel" },
+    { name: "Dagomon", imagePath: dagomon, artist: "Tortoiseshel" },
+    { name: "Digitamamon", imagePath: digitamamon, artist: "Tortoiseshel" },
+    { name: "Dracmon", imagePath: dracmon, artist: "Tortoiseshel" },
+    { name: "Duftmon", imagePath: duftmon, artist: "Tortoiseshel" },
+    { name: "Dukemon X", imagePath: dukemonX, artist: "Tortoiseshel" },
+    { name: "Duskmon", imagePath: duskmon, artist: "Tortoiseshel" },
+    { name: "Elecmon", imagePath: elecmon, artist: "Tortoiseshel" },
+    { name: "Etemon", imagePath: etemon, artist: "Tortoiseshel" },
+    { name: "Galgomon", imagePath: galgomon, artist: "Tortoiseshel" },
+    { name: "Gaogamon", imagePath: gaogamon, artist: "Tortoiseshel" },
+    { name: "Gaomon", imagePath: gaomon, artist: "Tortoiseshel" },
+    { name: "Garudamon", imagePath: garudamon, artist: "Tortoiseshel" },
+    { name: "Guardromon", imagePath: guardromon, artist: "Tortoiseshel" },
+    { name: "Hangyomon", imagePath: hangyomon, artist: "Tortoiseshel" },
+    { name: "Herissmon", imagePath: herissmon, artist: "Tortoiseshel" },
+    { name: "Holydramon", imagePath: holydramon, artist: "Tortoiseshel" },
+    { name: "IceDevimon", imagePath: iceDevimon, artist: "Tortoiseshel" },
+    { name: "Ikkakumon", imagePath: ikkakumon, artist: "Tortoiseshel" },
+    { name: "Jyureimon", imagePath: jyureimon, artist: "Tortoiseshel" },
+    { name: "Knightmon", imagePath: knightmon, artist: "Tortoiseshel" },
+    { name: "Koemon", imagePath: koemon, artist: "Tortoiseshel" },
+    { name: "Kougamon", imagePath: kougamon, artist: "Tortoiseshel" },
+    { name: "Lalamon", imagePath: lalamon, artist: "Tortoiseshel" },
+    { name: "Lucemon Falldown", imagePath: lucemonFalldown, artist: "Tortoiseshel" },
+    { name: "Mastemon", imagePath: mastemon, artist: "Tortoiseshel" },
+    { name: "MegaSeadramon X", imagePath: megaSeadramonX, artist: "Tortoiseshel" },
+    { name: "Megadramon", imagePath: megadramon, artist: "Tortoiseshel" },
+    { name: "MegaloGrowmon", imagePath: megaloGrowmon, artist: "Tortoiseshel" },
+    { name: "Megidramon", imagePath: megidramon, artist: "Tortoiseshel" },
+    { name: "Meramon", imagePath: meramon, artist: "Tortoiseshel" },
+    { name: "MetalGarurumon X", imagePath: metalGarurumonX, artist: "Tortoiseshel" },
+    { name: "MetalGreymon Virus", imagePath: metalGreymonVirus, artist: "Tortoiseshel" },
+    { name: "Minervamon X", imagePath: minervamonX, artist: "Tortoiseshel" },
+    { name: "Monzaemon", imagePath: monzaemon, artist: "Tortoiseshel" },
+    { name: "NoblePumpmon", imagePath: noblePumpmon, artist: "Tortoiseshel" },
+    { name: "Ophanimon Core", imagePath: ophanimonCore, artist: "Tortoiseshel" },
+    { name: "Ordinemon", imagePath: ordinemon, artist: "Tortoiseshel" },
+    { name: "Paledramon", imagePath: paledramon, artist: "Tortoiseshel" },
+    { name: "Palmon", imagePath: palmon, artist: "Tortoiseshel" },
+    { name: "Penmon", imagePath: penmon, artist: "Tortoiseshel" },
+    { name: "Plotmon", imagePath: plotmon, artist: "Tortoiseshel" },
+    { name: "Rapidmon Armor", imagePath: rapidmonArmor, artist: "Tortoiseshel" },
+    { name: "Renamon", imagePath: renamon, artist: "Tortoiseshel" },
+    { name: "Submarimon", imagePath: submarimon, artist: "Tortoiseshel" },
+    { name: "Tentomon", imagePath: tentomon, artist: "Tortoiseshel" },
+    { name: "ToyAgumon", imagePath: toyAgumon, artist: "Tortoiseshel" },
+    { name: "Turuiemon", imagePath: turuiemon, artist: "Tortoiseshel" },
+    { name: "UlforceV-dramon", imagePath: ulforceVdramon, artist: "Tortoiseshel" },
+    { name: "Valdurmon", imagePath: valdurmon, artist: "Tortoiseshel" },
+    { name: "Velgrmon", imagePath: velgrmon, artist: "Tortoiseshel" },
+    { name: "WarGreymon X", imagePath: warGreymonX, artist: "Tortoiseshel" },
+    { name: "Yatagaramon 2006", imagePath: yatagaramon2006, artist: "Tortoiseshel" },
     { name: "Angoramon", imagePath: angoramonSrc, artist: "Drak" },
     { name: "Elizamon", imagePath: elizamonSrc, artist: "Drak" },
     { name: "Gemmon", imagePath: gemmonSrc, artist: "Drak" },
@@ -137,6 +256,7 @@ export const avatars: Picture[] = [
     { name: "Shoemon", imagePath: shoemonSrc, artist: "Drak" },
     { name: "Namakemon", imagePath: namakemonSrc, artist: "Drak" },
     { name: "Pteromon", imagePath: pteromonSrc, artist: "Drak" },
+    { name: "Gekkomon", imagePath: gekkomon, artist: "Drak" },
     { name: "Mother", imagePath: motherSrc, artist: "Drak" },
     { name: "Searcher", imagePath: searcherSrc, artist: "Ruki" },
     { name: "Eater", imagePath: eaterSrc, artist: "Ruki" },
